bfda9bd81c8c134797627d80362207b334e90b4c
Ricardo Trevino <ricardotrevino24@gmail.com>
2023-01-02 15:32:53 -0600

    Create tabs and adds file structure

---
+++ RickAndMorty/Models/RMCharacter.swift
@@ -0,0 +1,8 @@
+//
+//  RMCharacter.swift
+//  RickAndMorty
+//
+//  Created by Ricardo Trevino on 1/2/23.
+//
+
+import Foundation
---
+++ RickAndMorty/Models/RMEpisode.swift
@@ -0,0 +1,8 @@
+//
+//  RMEpisode.swift
+//  RickAndMorty
+//
+//  Created by Ricardo Trevino on 1/2/23.
+//
+
+import Foundation
