5229083137e7579faccb94c11f29d230301abe78
Ricardo Trevino <ricardotrevino24@gmail.com>
2023-01-02 15:37:19 -0600

    Fixes M bug

---
+++ RickAndMorty/Controllers/Core/RMTabViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Ricardo Trevino on 1/2/23.
+//
+
+import UIKit
+
+class RMTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+    
+    
+}
