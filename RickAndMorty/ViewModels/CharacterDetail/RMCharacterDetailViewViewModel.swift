b1675db6665173aa155978abee7318f9b571c476
Ricardo Trevino <ricardotrevino24@gmail.com>
2023-01-20 11:13:35 -0600

    Adds data to info InfoCollectionViewCell

--- RickAndMorty/ViewModels/CharacterDetail/RMCharacterDetailViewViewModel.swift
+++ RickAndMorty/ViewModels/CharacterDetail/RMCharacterDetailViewViewModel.swift
@@ -27,31 +27,19 @@ final class RMCharacterDetailViewViewModel {
     }
     
     private func setUpSections(){
-        /*
-         let status: RMCharacterStatus
-         let species: String
-         let type: String
-         let gender: RMCharacterGender
-         let origin: RMOrigin
-         let location: RMSingleLocation
-         let image: String
-         let episode: [String]
-         let url: String
-         let created: String
-         */
-        
+    
         
         sections = [
             .photo(viewModel: .init(imageUrl: URL(string: character.image))),
             .information(viewModels: [
-                .init(value: character.status.text, title: "Status"),
-                .init(value: character.gender.rawValue, title: "Gender"),
-                .init(value: character.type, title: "Type"),
-                .init(value: character.species, title: "Species"),
-                .init(value: character.origin.name, title: "Origin"),
-                .init(value: character.location.name, title: "Location"),
-                .init(value: character.created, title: "Created"),
-                .init(value: "\(character.episode.count)", title: "Total Episodes")
+                .init(type: .status , value: character.status.text),
+                .init(type: .gender , value: character.gender.rawValue),
+                .init(type: .type , value: character.type),
+                .init(type: .species , value: character.species),
+                .init(type: .origin , value: character.origin.name),
+                .init(type: .location , value: character.location.name),
+                .init(type: .created , value: character.created),
+                .init(type: .episodeCount , value: "\(character.episode.count)")
                 
             ]),
             .episodes(viewModels: character.episode.compactMap ({
